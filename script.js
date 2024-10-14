ce708b3294cec7ea0571a930097d7d9f961978c8
Elias G <117037593+el19g3@users.noreply.github.com>
2024-10-14 22:36:52 +0300

    Add flexbox properties in container and squareDivs

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const numberOfSquareDivs = 16*16;
 for (let i = 0; i < numberOfSquareDivs; i++) {
       const squareDiv = document.createElement("div");
       squareDiv.id = 'squareDiv';
-      squareDiv.style.width = '100px';
-      squareDiv.style.height = '100px';
-      squareDiv.style.border = '1px solid black';
       container.appendChild(squareDiv);
 }
 
