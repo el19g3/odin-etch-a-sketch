eddf57f98595bbbd3617286f77591e000664712d
Elias G <117037593+el19g3@users.noreply.github.com>
2024-10-14 21:42:59 +0300

    Create loop for all the square divs inside container

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const squareDiv = document.createElement("div");
-
-squareDiv.id = 'squareDiv';
-squareDiv.style.width = '100px';
-squareDiv.style.height = '100px';
-squareDiv.style.border = '1px solid black';
-
-
-
 const container = document.getElementById('container');
 
-container.appendChild(squareDiv);
+const numberOfSquareDivs = 16*16;
+
+for (let i = 0; i < numberOfSquareDivs; i++) {
+      const squareDiv = document.createElement("div");
+      squareDiv.id = 'squareDiv';
+      squareDiv.style.width = '100px';
+      squareDiv.style.height = '100px';
+      squareDiv.style.border = '1px solid black';
+      container.appendChild(squareDiv);
+}
+
