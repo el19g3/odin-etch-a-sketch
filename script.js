91da0f5d1d8e91f04d2cc8f66c8d79d3f1ad4f78
Elias G <117037593+el19g3@users.noreply.github.com>
2024-10-15 20:32:26 +0300

    Make some comments in the script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const container = document.getElementById('container');
+//Create the grid of square divs inside a "container"
+
+const container = document.getElementById("container");
 
 const numberOfSquareDivs = 16*16;
 
 for (let i = 0; i < numberOfSquareDivs; i++) {
       const squareDiv = document.createElement("div");
-      squareDiv.id = 'squareDiv';
+      squareDiv.id = "squareDiv";
       container.appendChild(squareDiv);
 }
+
+//Make the hover effect to the grid squares
+
+
 
