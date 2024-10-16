b09ad8831a87f62ce9cafcb99425d556ecdbcbae
Elias G <117037593+el19g3@users.noreply.github.com>
2024-10-16 10:38:25 +0300

    Change squareDiv to id and create hover effect

--- script.js
+++ script.js
@@ -2,15 +2,25 @@
 
 const container = document.getElementById("container");
 
-const numberOfSquareDivs = 16*16;
+const numberOfSquareDivs = 16 * 16;
 
 for (let i = 0; i < numberOfSquareDivs; i++) {
       const squareDiv = document.createElement("div");
-      squareDiv.id = "squareDiv";
+      squareDiv.className = "squareDiv";
       container.appendChild(squareDiv);
 }
 
 //Make the hover effect to the grid squares
+const hoverDivs = document.querySelectorAll(".squareDiv");
 
+hoverDivs.forEach(squareDiv => {
+      squareDiv.addEventListener("mouseover", () => {
+            squareDiv.style.backgroundColor = "orange";
+      });
+})
 
-
+hoverDivs.forEach(squareDiv => {
+      squareDiv.addEventListener("mouseout", () => {
+            squareDiv.style.backgroundColor = "white";
+      });
+})
